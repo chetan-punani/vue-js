28787a0a0e0920d5b72c2f52ea0ae7f8e9a4e5c3
chetan <chetan@gmailcom>
2022-02-25 22:01:55 +0530

    section 15

---
+++ section_15/vuex/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+<section>
+  <base-container title="Vuex" v-if="isAuth">
+    <the-counter></the-counter>
+    <favourite-value></favourite-value>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+
+  <base-container title="Auth">
+    <user-auth></user-auth>
+  </base-container>
+</section>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavouriteValue from './components/FavouriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavouriteValue,
+    UserAuth
+  },
+  computed: {
+        isAuth() {
+            return this.$store.getters.userIsAuthenticated;
+        }
+  },
+  methods: {
+    addOne() {
+      // this.$store.commit('increase', { value: 10 });
+            
+            this.$store.dispatch({
+                type: 'numbers/increase',
+                value: 10   
+            });
+    }
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ section_15/vuex/src/components/ChangeCounter.vue
@@ -0,0 +1,22 @@
+<template>
+<section>
+    <button @click="inc">Add 1</button>
+    <button @click="increase({value: 11})">Add 11</button>
+</section>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+    methods: {
+        // addOne() {
+        //     this.$store.dispatch('increment');
+        // }
+        // ...mapActions(['increment','increase'])
+        ...mapActions('numbers',{
+            inc: 'increment',
+            increase: 'increase'
+        })
+    }
+}
+</script>
