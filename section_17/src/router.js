d91dd7ae7c9feece77ab09f8bd5c2d0cfc4acfcc
chetan <chetan@gmailcom>
2022-03-03 19:55:42 +0530

    section 17 done.

--- section_17/src/router.js
+++ section_17/src/router.js
@@ -6,6 +6,8 @@ import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestReceived from './pages/requests/RequestReceived.vue';
 import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
+import store from './store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -18,10 +20,21 @@ const router = createRouter({
       props: true,
       children: [{ path: "contact", component: ContactCoach }],
     },
-    { path: "/register", component: CoachRegistration },
-    { path: "/requests", component: RequestReceived },
+    { path: "/register", component: CoachRegistration, meta: { requiresAuth: true } },
+    { path: "/requests", component: RequestReceived, meta: { requiresAuth: true } },
+    { path: "/auth", component: UserAuth, meta: { requiresUnauth: true } },
     { path: "/:notFount(.*)", component: NotFound },
   ],
+});
+
+router.beforeEach(function(to, _, next) {
+  if(to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if(to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
 });
 
 export default router;
