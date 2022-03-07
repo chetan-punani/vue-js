fc544337bde1e1ac8f988a8f076b7fae41008de9
chetan <chetan@gmailcom>
2022-03-07 22:04:12 +0530

    section 19 done.

---
+++ section_19/composition-api/src/components/UserData.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+</div>
+</template>
+
+<script>
+import { computed, inject } from 'vue';
+
+export default {
+    props: ['firstName', 'lastName'],
+    setup(argument) {
+        const uName = computed(function() {
+            return argument.firstName + ' ' + argument.lastName
+        });
+
+        const age = inject('userAge');
+        //context.emit('save-data', 1); //this.$emit('save-data', 1);
+
+        return { userName: uName, age: age};
+    }
+    // computed: {
+    //     userName() {
+    //         return this.firstName + ' ' + this.lastName;
+    //     }
+    // }
+}
+</script>
